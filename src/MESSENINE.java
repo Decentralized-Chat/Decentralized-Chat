03c5af3d13d0a3c1fc76d11934a94d27a8904b96
oldA5 <34305453+oldA5@users.noreply.github.com>
2018-06-22 16:38:32 +0800

    add title

--- src/MESSENINE.java
+++ src/MESSENINE.java
@@ -103,6 +103,7 @@ public class MESSENINE extends javax.swing.JFrame {
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Cheat Engine");
         setResizable(false);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
