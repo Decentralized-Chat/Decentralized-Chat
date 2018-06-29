0e1812b960808b339531ed95d26f1812df827b02
oldA5 <34305453+oldA5@users.noreply.github.com>
2018-06-29 13:43:57 +0800

    ..

--- src/MESSENINE.java
+++ src/MESSENINE.java
@@ -137,7 +137,7 @@ public class MESSENINE extends javax.swing.JFrame {
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Decentralized Chat");
+        setTitle("Cheat Engine");
         setResizable(false);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
